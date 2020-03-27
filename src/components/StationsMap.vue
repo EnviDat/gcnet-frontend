02699d46e32df870ca4a556469ed3979f829a512
Dominik Haas <shaodiese@gmail.com>
2020-03-27 11:04:15 +0100

    changed the height to 75vh

--- src/components/StationsMap.vue
+++ src/components/StationsMap.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-        :style="`height: ${iframeScreenHeight()}px; height: 80vh; overflow: auto;`"
+        :style="`height: ${iframeScreenHeight()}px; height: 75vh; overflow: auto;`"
         class="pa-1"
         id="map_svg"
         ref="map_svg"
