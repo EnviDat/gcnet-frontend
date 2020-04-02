5f4d41f6740d875baf01de51e8ba65c9e7ac6480
Dominik Haas <shaodiese@gmail.com>
2020-04-02 11:48:39 +0200

    splitup StatusIcon and StatusIconButton

--- src/components/BaseElements/BaseStatusIcon.vue
+++ src/components/BaseElements/BaseStatusIcon.vue
@@ -1,36 +1,10 @@
 <template>
 
-  <v-layout row
-            @mouseover="hoverBadge = true"
-            @mouseleave="hoverBadge = false">
-
-    <v-flex v-if="count > 0"
-            pa-0
-            style="z-index: 1;">
-      <v-badge v-bind="{ left: !hoverBadge }"
-                overlap
-                secondary
-                 >
-        <span slot="badge"
-              class="black--text"
-              secondary
-              style="font-size: 9px !important;">{{ count }}</span>
-      </v-badge>              
-    </v-flex>
-
-    <v-flex py-0 px-1 >
-      <v-btn icon small
-            class="ma-0"
-            :color="color"
-            style="height: 20px !important; width: 20px !important;"
-            @click="$emit('click')">
-        <v-icon small >{{ icon }}</v-icon>
-      </v-btn>
-      <!-- <img class="envidatIcon" :src="iconString" />                 -->
-    </v-flex>
-    
-  </v-layout>
-
+  <div :style="`background-color: ${color}; padding: 0px 2px; border-radius: 50%;`">
+    <v-icon small >
+      {{ icon }}
+    </v-icon>
+  </div>
 
 </template>
 
@@ -38,8 +12,13 @@
   export default {
     props: {
       icon: String,
-      count: Number,
-      color: String,      
+      size: {
+        type: Number,
+        default: () => {
+          return 20;
+        },
+      },
+      color: String,
     },
     data: () => ({
       hoverBadge: false,
