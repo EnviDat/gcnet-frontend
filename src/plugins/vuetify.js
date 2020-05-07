d7d4d9e517be2f6f325fd47c691c6b12e8f4c398
Dominik Haas <shaodiese@gmail.com>
2020-05-07 15:47:17 +0200

    changed theme colors;

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -8,14 +8,16 @@ import 'vuetify/src/stylus/app.styl';
 Vue.use(Vuetify, {
   iconfont: 'mdi',
   theme: {
-    primary: '#29B6F6', // light-blue lighten-1
-    secondary: '#81D4FA', // light-blue lighten-3 
-    accent: '#84FFFF', // cyan accent-1 
+    // primary: '#29B6F6', // light-blue lighten-1
+    primary: '#639BFF',
+    // secondary: '#81D4FA', // light-blue lighten-3 
+    secondary: '#77B5F8', // light-blue lighten-3 
+    accent: '#FABC57', // cyan accent-1 
     highlight: '#B2DFDB', // teal lighten-4
-    error: '#FF5252',
+    error: '#EE6363',
     errorHighlight: '#FF8A80', // red lighten-4
     info: '#2196F3',
-    success: '#4CAF50',
+    success: '#90D593',
     warning: '#FFC107',
   },
 });
